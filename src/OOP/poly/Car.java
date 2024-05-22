52b110b567969b5325988ec277e5c286b48cd222
Byeong Min Cho <merrybmc@gmail.com>
2024-05-22 15:29:19 +0900

    poly instanceof

---
+++ src/OOP/poly/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+    Tire tire;
+
+    public Car(Tire tire) {
+        this.tire = tire;
+    }
+
+    Tire getHankookTire() {
+        return new HankookTire("HANKOOK");
+    }
+
+    Tire getKiaTire() {
+        return new KiaTire("KIA");
+    }
+}
---
+++ src/OOP/poly/HankookTire.java
@@ -0,0 +1,11 @@
+public class HankookTire extends Tire {
+
+    public HankookTire(String company) {
+        super(company);
+    }
+
+    @Override
+    public void rideComfort() {
+        System.out.println(super.company + " 타이어 승차감은 " + 100);
+    }
+}
